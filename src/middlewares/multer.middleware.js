c57e6789063c5b2bb725289cc001f6a93eb90b17
rameshneel <ramesh@neelnetworks.com>
2024-05-07 15:30:32 +0530

    Update multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -8,7 +8,7 @@ const allowedFileTypes = ['.jpg', '.jpeg', '.png', '.webp'];
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public");
+        cb(null, "./Public");
     },
     filename: function (req, file, cb) {
               const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9); 
