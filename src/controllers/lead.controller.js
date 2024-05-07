0f4506363d159fbfe05c5ae6cae923596168bd18
rameshneel <ramesh@neelnewtworks.com>
2024-05-07 14:23:49 +0530

    some

--- src/controllers/lead.controller.js
+++ src/controllers/lead.controller.js
@@ -3,6 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import Lead from "../models/lead.model.js";
+import { isValidObjectId } from "mongoose";
 
 export const addLead = asyncHandler(async (req, res) => {
   const { customer_id} = req.params;
