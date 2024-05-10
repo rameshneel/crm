0bf04786811dd62894970775d439fa4d19faea20
rameshneel <ramesh@neelnetworks.com>
2024-05-10 13:17:36 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 import { ApiError } from "./utils/ApiError.js"
 const app = express()
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
-
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
+app.use(cors())
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("Public"))
