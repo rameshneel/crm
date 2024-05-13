a3233c8914f725ba04dd7eccc9f7c1c9539ef8fb
rameshneel <ramesh@neelnetworks.com>
2024-05-13 18:27:56 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import { ApiError } from "./utils/ApiError.js"
 const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
+    credentials: true,
+     allowedHeaders: ['Content-Type', 'cookie']
 }))
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
