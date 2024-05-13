5f5b26370b157c816426dd647a14e1d9d9f2ce0e
rameshneel <ramesh@neelnetworks.com>
2024-05-13 16:53:06 +0530

    Update auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,6 +5,8 @@ import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
+        console.log(req.cookies);
+        req.cookies(req);
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
         
         // console.log(token);
