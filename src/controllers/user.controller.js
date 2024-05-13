95c733c48dde3c73b5a0a08b033e8c4a241b121c
rameshneel <ramesh@neelnetworks.com>
2024-05-13 16:10:38 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -112,7 +112,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const options = {
      httpOnly: true,
-      secure:true,
+      secure:false,
       SameSite:"None",
       maxAge: 900000
   };
