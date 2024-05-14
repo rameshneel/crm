31af51293fd396e3e61ae7c99d7d2ed2959a51eb
rameshneel <ramesh@neelnetworks.com>
2024-05-14 11:38:03 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
+ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -114,7 +114,7 @@ const loginUser = asyncHandler(async (req, res) => {
      httpOnly: true,
       secure:true,
       // secure: false,
-      SameSite:"None",
+      SameSite:"Lax",
       maxAge: 900000
   };
 
