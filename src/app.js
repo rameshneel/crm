176f4462051af55e167560ccb0bacdf4517bbbe9
rameshneel <ramesh@neelnewtworks.com>
2024-05-15 15:24:09 +0530

    stackadderror

--- src/app.js
+++ src/app.js
@@ -50,7 +50,8 @@ function errorHandler(err, req, res, next) {
       return res.status(err.statusCode).json({
           success: false,
           message: err.message,
-          errors: err.errors
+          errors: err.errors,
+          stack:err.stack
       });
   }
   console.error('Internal Server Error:', err);
