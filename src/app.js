7962db737a1e6259c3c3d4fa5cb201b7109f3973
Ramesh151 <rkmahto151@gmail.com>
2024-05-20 22:51:25 +0530

    hi newcustomers

--- src/app.js
+++ src/app.js
@@ -1,39 +1,45 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import { ApiError } from "./utils/ApiError.js"
-const app = express()
-app.use(cors({
-    origin: ['http://localhost:3000','https://localhost:3000','http://localhost:5173'],
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { ApiError } from "./utils/ApiError.js";
+const app = express();
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://localhost:3000",
+      "http://localhost:5173",
+    ],
     credentials: true,
     secure: false,
-     optionSuccessStatus: 200,
+    optionSuccessStatus: 200,
     Headers: true,
-    exposedHeaders: 'Set-Cookie',
-    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-   allowedHeaders: [
-    'Access-Control-Allow-Origin',
-    'Content-Type',
-    'Authorization'
-  ]
-}))
+    exposedHeaders: "Set-Cookie",
+    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Access-Control-Allow-Origin",
+      "Content-Type",
+      "Authorization",
+    ],
+  })
+);
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("Public"))
-app.use(cookieParser())
-
-
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("Public"));
+app.use(cookieParser());
 
 //routes import
-import userRouter from './routes/user.routes.js'
+import userRouter from "./routes/user.routes.js";
 import customerRoutes from "./routes/customer.route.js";
-import leadRoutes from "./routes/lead.routes.js"
+import leadRoutes from "./routes/lead.routes.js";
+import newCustomerRoutes from "./routes/newCustomer.routes.js";
 
 //routes declaration
-app.use("/api/users", userRouter)
+app.use("/api/users", userRouter);
 app.use("/api/customers", customerRoutes);
 app.use("/api/leads", leadRoutes);
+app.use("/api/newcustomers", newCustomerRoutes);
 
 // app.use((err, req, res, next) => {
 //     console.log(err.stack);
@@ -42,29 +48,29 @@ app.use("/api/leads", leadRoutes);
 
 function errorHandler(err, req, res, next) {
   if (err instanceof ApiError) {
-      console.error(`API Error: ${err.message}`);
-      if (err.errors.length > 0) {
-          console.error('Validation Errors:', err.errors);
-      }
-      
-      return res.status(err.statusCode).json({
-          success: false,
-          message: err.message,
-          errors: err.errors,
-          stack:err.stack
-      });
-  }
-  console.error('Internal Server Error:', err);
-  return res.status(500).json({
+    console.error(`API Error: ${err.message}`);
+    if (err.errors.length > 0) {
+      console.error("Validation Errors:", err.errors);
+    }
+
+    return res.status(err.statusCode).json({
       success: false,
-      message: err.message ,
+      message: err.message,
+      errors: err.errors,
+      stack: err.stack,
+    });
+  }
+  console.error("Internal Server Error:", err);
+  return res.status(500).json({
+    success: false,
+    message: err.message,
   });
 }
 
-app.use(errorHandler); 
+app.use(errorHandler);
 
 app.use((req, res) => {
-    res.status(404).json({ message: "No route found" });
-  }); 
+  res.status(404).json({ message: "No route found" });
+});
 
-export { app }
+export { app };
