ab242896c56a6d303c44cfce20645a2ed1300ab9
rameshneel <ramesh@neelnewtworks.com>
2024-05-24 18:20:26 +0530

    router override error solved

--- src/routes/amendment.routes.js
+++ src/routes/amendment.routes.js
@@ -3,6 +3,7 @@ import {
   addAmendment,
   getAllAmendment,
   getAmendmentById,
+  getAmendmentsByStatus,
   updateAmendment
 } from "../controllers/Amendment.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -10,9 +11,25 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 router.use(verifyJWT); 
 
-router.route("/:customerId").post(addAmendment)
-router.route("/").get(getAllAmendment);
-router.route("/:amendmentId").get(getAmendmentById);
-router.route("/:amendmentId").patch(updateAmendment);
+// router.route("/:customerId").post(addAmendment)
+// router.route("/").get(getAllAmendment);
+// router.route("/:amendmentId").get(getAmendmentById);
+// router.route("/:amendmentId").patch(updateAmendment);
+// router.get('/', getAmendmentsByStatus);
+
+router.route('/id/:amendmentId')
+  .get(getAmendmentById)
+  .patch(updateAmendment);
+
+  router.route('/:customerId')
+  .post(addAmendment);
+
+  router.route('/status')
+  // .get(getAllAmendment)
+  .get(getAmendmentsByStatus);
+
+  router.route('/list')
+  .get(getAllAmendment)
+  // .get(getAmendmentsByStatus);
 
 export default router;
