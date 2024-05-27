09c2e26a71a8d4acc9e24b8bb4ae3e23f24087ce
rameshneel <ramesh@neelnewtworks.com>
2024-05-27 18:42:25 +0530

    leaddeatils update

--- src/controllers/lead.controller.js
+++ src/controllers/lead.controller.js
@@ -15,18 +15,7 @@ export const addLead = asyncHandler(async (req, res, next) => {
   }
   
   try {
-    const customer = await Customer.findByIdAndUpdate(
-      customer_id, {
-        $set: {
-          hasLead: true
-        }
-      }, { new: true }
-    );
-
-    if (!customer) {
-      return next(new ApiError(404, "Customer does not exist"));
-    }
-
+   
     const { lead_type, existing_website, outcome, orderforced, notes, status } = req.body;
     const user = await User.findById(userId);
     if (!user) {
@@ -81,55 +70,55 @@ export const getAllLeads = asyncHandler(async (req, res, next) => {
   }
 });
 
-export const LeadDetails = asyncHandler(async (req, res, next) => {
-  const { lead_id } = req.params;
-  if (!isValidObjectId(lead_id)) {
-    return next(new ApiError(400, "Invalid lead_id"));
-  }
+// export const LeadDetails = asyncHandler(async (req, res, next) => {
+//   const { lead_id } = req.params;
+//   if (!isValidObjectId(lead_id)) {
+//     return next(new ApiError(400, "Invalid lead_id"));
+//   }
 
-  try {
-    const objectId = new mongoose.Types.ObjectId(lead_id);
+//   try {
+//     const objectId = new mongoose.Types.ObjectId(lead_id);
 
-    const lead = await Lead.aggregate([
-      { $match: { _id: objectId } },
-      {
-        $lookup: {
-          from: 'customers',
-          localField: 'customer_id',
-          foreignField: '_id',
-          as: 'customer'
-        }
-      },
-      { $unwind: '$customer' },
-      {
-        $lookup: {
-          from: 'users',
-          localField: 'generated_by',
-          foreignField: '_id',
-          as: 'user'
-        }
-      },
-      { $unwind: '$user' },
-      {
-        $project: {
-          _id: 0,
-          companyName: '$customer.companyName',
-          contactName: '$customer.contactName',
-          Address: '$customer.address',
-          representativeName: '$user.fullName'
-        }
-      }
-    ]);
+//     const lead = await Lead.aggregate([
+//       { $match: { _id: objectId } },
+//       {
+//         $lookup: {
+//           from: 'customers',
+//           localField: 'customer_id',
+//           foreignField: '_id',
+//           as: 'customer'
+//         }
+//       },
+//       { $unwind: '$customer' },
+//       {
+//         $lookup: {
+//           from: 'users',
+//           localField: 'generated_by',
+//           foreignField: '_id',
+//           as: 'user'
+//         }
+//       },
+//       { $unwind: '$user' },
+//       {
+//         $project: {
+//           _id: 0,
+//           companyName: '$customer.companyName',
+//           contactName: '$customer.contactName',
+//           Address: '$customer.address',
+//           representativeName: '$user.fullName'
+//         }
+//       }
+//     ]);
 
-    if (!lead.length) {
-      return next(new ApiError(404, "Lead not found"));
-    }
+//     if (!lead.length) {
+//       return next(new ApiError(404, "Lead not found"));
+//     }
 
-    return res.status(200).json(new ApiResponse(200, lead[0], "Lead fetched successfully"));
-  } catch (error) {
-    return next(error);
-  }
-});
+//     return res.status(200).json(new ApiResponse(200, lead[0], "Lead fetched successfully"));
+//   } catch (error) {
+//     return next(error);
+//   }
+// });
 
 export const updateLead = asyncHandler(async (req, res, next) => {
   const { lead_id } = req.params;
@@ -201,36 +190,34 @@ export const deleteLead = asyncHandler(async (req, res, next) => {
 });
 
 
-// // export const LeadDetails = asyncHandler(async (req, res) => {
-// //   const { lead_id } = req.params;
-// //   if (!isValidObjectId(lead_id)) {
-// //     throw new ApiError(400, "Invalid lead_id");
-// //   }
-// //   try {
-// //     const leads = await Lead.findById(lead_id)
-// //     console.log(leads);
-// //     const lead = await Lead.findById(lead_id)
-// //       .populate({
-// //         path: 'customer_id',
-// //         select: 'companyName contactName'
-// //       })
-// //       .populate({
-// //         path: 'generated_by',
-// //         select: 'fullName'
-// //       });
-// //   console.log("lead",lead);
-// //     if (!lead) {
-// //       throw new ApiError("Lead Not Found!");
-// //     }
 
-// //     // Extract necessary fields from the populated data
-// //     const { companyName, contactName } = lead.customer_id;
-// //     const { fullName } = lead.generated_by;
+export const LeadDetails = asyncHandler(async (req, res,next) => {
+  const { lead_id } = req.params;
+  if (!isValidObjectId(lead_id)) {
+    throw new ApiError(400, "Invalid lead_id");
+  }
+  try {
+    const leads = await Lead.findById(lead_id)
+    console.log(leads);
+    const lead = await Lead.findById(lead_id)
+      .populate({
+        path: 'customer_id',
+      })
+      .populate({
+        path: 'generated_by',
+      });
+  console.log("lead",lead);
+    if (!lead) {
+      throw new ApiError("Lead Not Found!");
+    }
 
-// //     // Now you can use companyName, contactName, and fullName as needed
-// //     res.status(200).json({ companyName, contactName, fullName });
-// //   } catch (error) {
-// //     console.error(error);
-// //     return res.status(500).json({ message: "Internal Server Error" });
-// //   }
-// // });
+    // Extract necessary fields from the populated data
+    const { companyName, contactName } = lead.customer_id;
+    const { fullName } = lead.generated_by;
+
+    // Now you can use companyName, contactName, and fullName as needed
+    return res.status(200).json(new ApiResponse(200, lead, "Leads retrieved successfully"));
+  } catch (error) {
+   next(error)
+  }
+});
