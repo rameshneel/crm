5a8636186f2375b19a67beffd66458d5b5d5e86b
rameshneel <ramesh@neelnewtworks.com>
2024-06-03 19:19:09 +0530

    customer signature

--- src/helper/multererror.js
+++ src/helper/multererror.js
@@ -30,3 +30,18 @@ export const uploadFile = (req, res, next) => {
       next()
     });
   };
+
+
+  export const uploadFile2 = (req, res, next) => {
+    console.log(multer.MulterError);
+    upload.single('customerSignature')(req, res, (err) => {
+      if (err instanceof multer.MulterError) {
+        if (err.code === 'LIMIT_FILE_SIZE') {
+          return res.status(400).json({ message: 'File size exceeds limit of 1MB' });
+        }
+      } else if (err) {
+        return res.status(400).json({ message: err.message });
+      }
+      next()
+    });
+  };
