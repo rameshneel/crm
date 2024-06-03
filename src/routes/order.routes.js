126ac1e3b423218b3c31d285d802b36d3b6cba67
rameshneel <ramesh@neelnewtworks.com>
2024-06-03 19:15:58 +0530

    order route

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { addOrder, deleteOrder, getAllOrders, getOrderById, updateOrder } from '../controllers/order.controllers.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { uploadFile2 } from '../helper/multererror.js';
 
 const router = express.Router();
 router.use(verifyJWT); 
 
-router.post('/add/:customer_id', addOrder);
+router.post('/add/:customer_id',uploadFile2, addOrder);
 router.get('/', getAllOrders);
 router.get("/:order_id",getOrderById)
 router.patch('/update/:order_id',updateOrder)
