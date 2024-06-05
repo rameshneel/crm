2018902e6b45f2a291423b48ec7cbef7a55f4d6d
rameshneel <ramesh@neelnewtworks.com>
2024-06-05 12:04:15 +0530

    appointments route update

--- src/routes/lead.routes.js
+++ src/routes/lead.routes.js
@@ -20,6 +20,6 @@ router.post("/appointments/:lead_id", addAppointment);
 router.delete("/appointments/:appointment_id", deleteAppointment);
 router.patch("/appointments/update/:appointment_id", updateAppointment);
 router.get("/lead/appointments/:lead_id", getAppointmentsByDate);
-router.get("/",getAllAppointments);
+router.get("/appointments",getAllAppointments);
 
 export default router;
