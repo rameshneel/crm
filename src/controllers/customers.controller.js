ac1c700eaa5bf5403575ba9880d44e72329fb6cd
rameshneel <ramesh@neelnewtworks.com>
2024-06-18 11:21:41 +0530

    cutomers delete controller update

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -289,158 +289,35 @@ const updateCustomer = asyncHandler(async (req, res, next) => {
   }
 });
 
-// const updateCustomer = asyncHandler(async (req, res, next) => {
-//   const { customer_id } = req.params;
-//   const userId = req.user?._id;
-
-//   if (!isValidObjectId(customer_id)) {
-//     return next(new ApiError(400, "Invalid customer_id"));
-//   }
-  
-//   try {
-//     const {
-//       companyName,
-//       contactName,
-//       mobileNo,
-//       landlineNo,
-//       streetNoName,
-//       town,
-//       county,
-//       customerEmail,
-//       postcode,
-//       url,
-//       address,
-//       status,
-//       liveDate,
-//       ssl,
-//       sitemap,
-//       htAccess,
-//       gaCode,
-//       newGACode,
-//       ordersRenewals,
-//     } = req.body;
-      
-// if (
-//   ![customerEmail, status].some((field) => {
-//     if (field === undefined) return false;
-//     if (typeof field === "string") return field.trim() !== "";
-//   })
-// ) {
-//   throw new ApiError(400, "At least one field is required for update");
-// }
-
-//     let avatarurl = "";
-
-//     // Check if file is uploaded
-//     if (req.file && req.file.path) {
-//       const avatarLocalPath = req.file.path;
-
-//       // Handle file upload
-//       try {
-//         const formData = new FormData();
-//         formData.append("file", fs.createReadStream(avatarLocalPath));
-//         const apiURL = "https://crm.neelnetworks.org/public/file_upload/api.php";
-//         const apiResponse = await axios.post(apiURL, formData, {
-//           headers: {
-//             ...formData.getHeaders(),
-//           },
-//         });
-        
-//         avatarurl = apiResponse.data?.img_upload_path;
-//         if (!avatarurl) {
-//           throw new Error("img_upload_path not found in API response");
-//         }
-
-//         // Remove local avatar file after upload
-//         fs.unlink(avatarLocalPath, (err) => {
-//           if (err) {
-//             console.error("Error removing avatar file:", err.message);
-//           } else {
-//             console.log("Avatar file removed successfully");
-//           }
-//         });
-//       } catch (error) {
-//         // Handle avatar upload error
-//         console.error("Error uploading avatar:", error.message);
-//         fs.unlinkSync(avatarLocalPath); // Delete local file on error
-//         throw new ApiError(500, "Avatar upload failed");
-//       }
-//     }
-
-//     // Find user by userId
-//     const user = await User.findById(userId);
-//     if (!user) {
-//       throw new ApiError(404, "User does not exist");
-//     }
-
-//     // Find customer by customer_id
-//     const customer = await Customer.findById(customer_id);
-//     if (!customer) {
-//       throw new ApiError(404, "Customer not found");
-//     }
-
-//     // Check user authorization
-//     if (user.role !== "admin" && customer.createdBy.toString() !== userId) {
-//       throw new ApiError(401, "Unauthorized request");
-//     }
-
-//     // Update customer properties
-//     customer.companyName = companyName;
-//     customer.contactName = contactName;
-//     customer.mobileNo = mobileNo;
-//     customer.landlineNo = landlineNo;
-//     customer.streetNoName = streetNoName;
-//     customer.town = town;
-//     customer.county = county;
-//     customer.customerEmail = customerEmail;
-//     customer.postcode = postcode;
-//     customer.url = url;
-//     customer.address = address;
-//     customer.status = status;
-//     customer.liveDate = liveDate;
-//     customer.ssl = ssl;
-//     customer.sitemap = sitemap;
-//     customer.htAccess = htAccess;
-//     customer.gaCode = gaCode;
-//     customer.newGACode = newGACode;
-//     customer.ordersRenewals = ordersRenewals;
-//     customer.logo = avatarurl;
-//     customer.updatedBy = userId;
-
-//     // Save updated customer
-//     const updatedCustomer = await customer.save();
-
-//     // Send response
-//     return res.status(200).json(new ApiResponse(200, updatedCustomer, "Customer updated successfully"));
-//   } catch (error) {
-//     return next(error);
-//   }
-// });
-
 const deleteCustomer = asyncHandler(async (req, res, next) => {
   try {
-    const activeUser = req.user?._id;
-    const user = await User.findById(activeUser);
-    const customerId = req.params.customerId;
+   
+    const activeUserId = req.user?._id;
+    const user = await User.findById(activeUserId);
+    
+    const { customerId } = req.params;
+
     const customer = await Customer.findById(customerId);
 
     if (!customer) {
-      throw new ApiError(404, "Customer not found");
+      throw new ApiError(404, 'Customer not found');
     }
 
-    if (
-      user.role === "admin" ||
-      (user.role === "salesman" && customer.createdBy.equals(activeUser))
-    ) {
+    if (user.role === 'admin' || (user.role === 'salesman' && customer.createdBy.equals(activeUserId))) {
+      
       await Customer.findByIdAndDelete(customerId);
-      return res.status(204).end();
+      return res
+      .status(200)
+      .json(new ApiResponse(204, {}, "Customer deleted successfully"));
+    
     } else {
-      throw new ApiError(401, "Unauthorized");
+      throw new ApiError(401, 'Unauthorized');
     }
   } catch (error) {
     return next(error);
   }
 });
+
 const getCustomerById = asyncHandler(async (req, res, next) => {
   try {
     const { customerId } = req.params;
@@ -466,12 +343,12 @@ const getCustomerById = asyncHandler(async (req, res, next) => {
 //update for customers
 
  const createCustomerUpdate = asyncHandler(async (req, res,next) => {
-  console.log("jh");
     const userId = req.user?._id;
     const { customerId } = req.params;
   try {
-  
+   
     const { content,files, mentions } = req.body;
+   
     const update = new Update({
       content,
       createdBy: userId,
