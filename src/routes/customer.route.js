b2f26bed98cf2bbc2d94636105530a7bd6f0a522
rameshneel <ramesh@neelnewtworks.com>
2024-06-18 10:44:40 +0530

    customer delete route add

--- src/routes/customer.route.js
+++ src/routes/customer.route.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { createCustomer, createCustomerUpdate, customerList, deleteUpdate, getAllUpdates, getCustomerById, replyToUpdate, toggleLike, updateCustomer, updateUpdate } from "../controllers/customers.controller.js";
+import { createCustomer, createCustomerUpdate, customerList, deleteCustomer, deleteUpdate, getAllUpdates, getCustomerById, replyToUpdate, toggleLike, updateCustomer, updateUpdate } from "../controllers/customers.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { uploadFile1 } from "../helper/multererror.js";
+import {  uploadFile1 } from "../helper/multererror.js";
 
 const router = express.Router();
 router.use(verifyJWT); 
 
 router.post("/",uploadFile1, createCustomer);
 router.get("/:customerId",getCustomerById);
+router.delete("/:customerId",deleteCustomer);
 router.get("/",customerList);
 router.patch("/update/:customer_id",uploadFile1,updateCustomer);
 
@@ -19,8 +20,8 @@ router.post('/updates/:updateId/reply', replyToUpdate)
 router.post('/updates/:updateId/like', toggleLike)
 router.patch('/:updateId',updateUpdate)
 router.delete('/:updateId',deleteUpdate)
-//   .patch(updateUpdate) 
-//   .delete(deleteUpdate);
+//    .patch(updateUpdate) 
+//   .delete(deleteUpdate)
 
 export default router;
 
