0d9666fc881fbfd42dad5780b7d5502c86ff0fe9
rameshneel <ramesh@neelnewtworks.com>
2024-06-19 15:09:59 +0530

    order routes update route /delete  /:orderid

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addOrder, deleteOrder, getAllOrders, getOrderById, updateOrder } from '../controllers/order.controllers.js';
+import { addOrder, createOrderUpdate, deleteOrder, getAllOrderUpdates, getAllOrders, getOrderById, updateOrder } from '../controllers/order.controllers.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 
@@ -10,6 +10,18 @@ router.post('/add/:customer_id',addOrder);
 router.get('/', getAllOrders);
 router.get("/:order_id",getOrderById)
 router.patch('/update/:order_id', updateOrder)
-router.delete('/delete/:order_id',deleteOrder);
+router.delete('/:order_id',deleteOrder);
+
+
+//update routes
+
+router.get('/updates/all/:orderId', getAllOrderUpdates);
+router.post('/:orderId/updates', createOrderUpdate);
+// router.post('/updates/:updateId/reply', replyToUpdate)
+// router.post('/updates/:updateId/like', toggleLike)
+// router.patch('/:updateId',updateUpdate)
+// router.delete('/:updateId',deleteUpdate)
+//    .patch(updateUpdate) 
+//   .delete(deleteUpdate)
 
 export default router;
