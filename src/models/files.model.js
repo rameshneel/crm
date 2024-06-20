350769276436c74dc1c78d48c0e99dba068e9519
rameshneel <ramesh@neelnewtworks.com>
2024-06-20 16:22:27 +0530

    file model upload

---
+++ src/models/files.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const fileSchema = new Schema({
+  url: {
+    type: String,
+    required: true,
+  },
+  uploadedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  uploadedAt: {
+    type: Date,
+    default: Date.now,
+  }, 
+
+  itemType: {
+    type: String,
+    required: true,
+    enum: ["Customer", "Order", "Lead", "Amendment"],
+  },
+  itemId: {
+    type: mongoose.Schema.Types.ObjectId,
+    refPath: "itemType",
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+    enum: ["UpdateFile", "FileGallery"],
+  },
+  
+},{ timestamps: true },);
+
+const File = mongoose.model('File', fileSchema);
+export default File;
