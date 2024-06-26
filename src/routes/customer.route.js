edef3bafacc4960405b74910f7e7a030b5ae1034
rameshneel <ramesh@neelnewtworks.com>
2024-06-26 13:18:11 +0530

    customer routes

--- src/routes/customer.route.js
+++ src/routes/customer.route.js
@@ -1,28 +1,46 @@
 import express from "express";
-import { createCustomer, createCustomerUpdate, customerList, deleteCustomer, deleteUpdate, getAllUpdates, getCustomerById, replyToUpdate, toggleLike, updateCustomer, updateUpdate } from "../controllers/customers.controller.js";
+import {
+  createCustomer,
+  createCustomerUpdate,
+  customerList,
+  deleteCustomer,
+  deleteUpdate,
+  getAllCustomerUpdates,
+  getCustomerById,
+  getUpdateById,
+  getallFilesforCustomers,
+  replyToUpdate,
+  toggleLike,
+  updateCustomer,
+  updateUpdate,
+  uploadFilesToGalleryforCustomers,
+} from "../controllers/customers.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import {  uploadFile1 } from "../helper/multererror.js";
+import { fileUpload, fileUploadGallery, uploadFile1 } from "../helper/multererror.js";
+
 
 const router = express.Router();
-router.use(verifyJWT); 
+router.use(verifyJWT);
 
-router.post("/",uploadFile1, createCustomer);
-router.get("/:customerId",getCustomerById);
-router.delete("/:customerId",deleteCustomer);
-router.get("/",customerList);
-router.patch("/update/:customer_id",uploadFile1,updateCustomer);
+router.post("/", uploadFile1, createCustomer);
+router.get("/:customerId", getCustomerById);
+router.delete("/:customerId", deleteCustomer);
+router.get("/", customerList);
+router.patch("/update/:customer_id", uploadFile1, updateCustomer);
 
 //update routes
 
-router.get('/updates/all/:customerId', getAllUpdates);
-router.post('/:customerId/updates', createCustomerUpdate);
-router.post('/updates/:updateId/reply', replyToUpdate)
-router.post('/updates/:updateId/like', toggleLike)
-router.patch('/:updateId',updateUpdate)
-router.delete('/:updateId',deleteUpdate)
-//    .patch(updateUpdate) 
-//   .delete(deleteUpdate)
+router.get("/updates/all/:customerId", getAllCustomerUpdates);
+router.get("/updates/:updateId", getUpdateById);
+router.post("/:customerId/updates", fileUpload, createCustomerUpdate);
+router.post("/updates/:updateId/reply", replyToUpdate);
+router.post("/updates/:updateId/like", toggleLike);
+router.patch("/:updateId", updateUpdate);
+router.delete("/:updateId", deleteUpdate);
+
+//for file
+router.get("/file/:customerId", getallFilesforCustomers);
+router.post("/file/:customerId",fileUploadGallery, uploadFilesToGalleryforCustomers);
+
 
 export default router;
-
-
