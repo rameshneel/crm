e2f94fca7940694b3d0a0dba1e6a86fea88db6e0
rameshneel <ramesh@neelnewtworks.com>
2024-06-27 14:59:02 +0530

    bgb

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { addOrder, createOrderUpdate, deleteOrder, deleteUpdateforOrder, getAllOrderUpdates, getAllOrders, getOrderById, replyToUpdateforOrder, toggleLikeforOrder, updateOrder, updateUpdateforOrder } from '../controllers/order.controllers.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { replyToUpdate } from '../controllers/updates.controllers.js';
 
 
 const router = express.Router();
 router.use(verifyJWT); 
 
-router.post('/add/:customer_id',addOrder);
+router.post('/:customer_id',addOrder);
 router.get('/', getAllOrders);
 router.get("/:order_id",getOrderById)
 router.patch('/update/:order_id', updateOrder)
@@ -17,7 +18,7 @@ router.delete('/:order_id',deleteOrder);
 
 router.get('/updates/all/:orderId', getAllOrderUpdates);
 router.post('/:orderId/updates', createOrderUpdate);
-router.post('/updates/:updateId/reply', replyToUpdateforOrder)
+router.post('/:updateId/reply', replyToUpdate)
 router.post('/updates/:updateId/like', toggleLikeforOrder)
 router.patch('/:updateId',updateUpdateforOrder)
 router.delete('/:updateId',deleteUpdateforOrder)
