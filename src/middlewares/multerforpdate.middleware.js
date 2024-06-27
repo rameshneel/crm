7b7ec6f3eae44c8e791bcc7bb60c0e67bd3fd068
rameshneel <ramesh@neelnewtworks.com>
2024-06-27 11:51:19 +0530

    multer error

---
+++ src/middlewares/multerforpdate.middleware.js
@@ -0,0 +1,32 @@
+import multer from "multer";
+import path from "path";
+
+const allowedFileTypes = ['.jpg', '.jpeg', '.png', '.pdf', '.doc', '.docx', '.xls', '.xlsx',".txt"];
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./Public");
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Math.round(Math.random()*1E9); 
+        const randomFilename = uniqueSuffix + '-' + file.originalname; 
+        cb(null, randomFilename); 
+    }
+});
+
+const limits = { fileSize: 1024*1000};
+
+const fileFilter = (req, file, cb) => {
+    const ext = path.extname(file.originalname).toLowerCase();
+    if (allowedFileTypes.includes(ext)) {
+        cb(null, true); 
+    } else {
+        cb(new Error('Only specific file types are allowed!'), false); 
+    }
+};
+
+export const uploadForUpdate = multer({ 
+    storage, 
+    limits,
+    fileFilter, 
+});
