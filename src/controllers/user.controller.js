7f703bfb1dac4206a06a31ec0b077ef26e9cb7ca
rameshneel <ramesh@neelnewtworks.com>
2024-07-02 18:11:29 +0530

    login secure true

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -225,7 +225,7 @@ const loginUser = asyncHandler(async (req, res,next) => {
   
     const options = {
        httpOnly: true,
-        // secure:true,
+        secure:true,
         // secure: false,
         sameSite: "none",
         // SameSite:"Lax",
