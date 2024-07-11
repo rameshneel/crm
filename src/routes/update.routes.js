6ac7d5f5d12e63a5331b4009b609443f519ddb30
rameshneel <ramesh@neelnewtworks.com>
2024-07-11 16:47:21 +0530

    toogle like update route

--- src/routes/update.routes.js
+++ src/routes/update.routes.js
@@ -8,7 +8,8 @@ import {
   toggleLike,
   updatePinnedStatus,
   deleteUpdate,
-  deleteReply
+  deleteReply,
+  updatedUpdate
 } from "../controllers/updates.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { fileUploadforupdate } from "../helper/multererror.js";
@@ -19,10 +20,11 @@ router.use(verifyJWT);
 router.post("/:entityType/:entityId", fileUploadforupdate, createEntityUpdate);
 router.get("/", getUpdateById);
 router.get("/:entityType/:entityId", getAllUpdatesForEntity);
-router.post("/:updateId/like", toggleLike);
+router.post("/toggle/:updateId/like", toggleLike);
 router.post("/update/reply/:updateId", replyToUpdate);
 router.patch("/:updateId/pin", updatePinnedStatus);
 router.patch("/:updateId/view", logUpdateView);
 router.delete("/:id",deleteUpdate)
 router.delete('/replies/:replyId',  deleteReply);
+router.patch('/:id', fileUploadforupdate, updatedUpdate);
 export default router;
