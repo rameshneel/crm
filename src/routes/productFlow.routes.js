0496d7055fc49703630142d4a43a0b5fd7dc106c
rameshneel <ramesh@neelnetworks.com>
2024-07-26 18:46:52 +0530

    missing customerId add

--- src/routes/productFlow.routes.js
+++ src/routes/productFlow.routes.js
@@ -9,7 +9,7 @@ import {
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 router.use(verifyJWT);
-router.post("/:", createProductFlow);
+router.post("/:customerId", createProductFlow);
 router.get("/", getProductFlows);
 router.get("/:id", getProductFlowById);
 router.patch("/:id", updateProductFlow);
