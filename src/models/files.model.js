bdef1415ad00c0f8c9b9c6798310cab05365aa16
rameshneel <ramesh@neelnetworks.com>
2024-07-30 15:39:45 +0530

    this update  for files

--- src/models/files.model.js
+++ src/models/files.model.js
@@ -1,38 +1,48 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const fileSchema = new Schema({
-  fileUrl: {
-    type: String,
-    required: true,
-  },
-  uploadedBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  uploadedAt: {
-    type: Date,
-    default: Date.now,
-  }, 
+const fileSchema = new Schema(
+  {
+    fileUrl: {
+      type: String,
+      required: true,
+    },
+    uploadedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    uploadedAt: {
+      type: Date,
+      default: Date.now,
+    },
 
-  itemType: {
-    type: String,
-    required: true,
-    enum: ["Customer", "Order", "Lead", "Amendment", "Update"],
+    itemType: {
+      type: String,
+      required: true,
+      enum: [
+        "Customer",
+        "Order",
+        "Lead",
+        "Amendment",
+        "Reply",
+        "Update",
+        "TechnicalTracker",
+      ],
+    },
+    itemId: {
+      type: mongoose.Schema.Types.ObjectId,
+      refPath: "itemType",
+      required: true,
+    },
+    source: {
+      type: String,
+      required: true,
+      enum: ["UpdateFile", "FileGallery", "ReplyFile"],
+    },
   },
-  itemId: {
-    type: mongoose.Schema.Types.ObjectId,
-    refPath: "itemType",
-    required: true,
-  },
-  source: {
-    type: String,
-    required: true,
-    enum: ["UpdateFile", "FileGallery","ReplyFile"],
-  },
-  
-},{ timestamps: true },);
+  { timestamps: true }
+);
 
-const File = mongoose.model('File', fileSchema);
+const File = mongoose.model("File", fileSchema);
 export default File;
