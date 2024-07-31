d09c65db15f411e804d8f5a7d4252dafcb6a364b
rameshneel <ramesh@neelnetworks.com>
2024-07-31 18:00:54 +0530

    copy wirter tracker model add

---
+++ src/models/copywriterTracker.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const copywriterTrackerSchema = new Schema(
+  {
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Customer",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    status: {
+      type: String,
+      enum: [
+        "Homepage In Process",
+        "Rework",
+        "Additional Pages in Process",
+        "Homepage Complete",
+        "Remaining Pages in Process",
+        "COMPLETED",
+        "In Query",
+        "Held for Critical",
+        "Waiting on Info",
+        "COMPLETED REWORK",
+        "Area Pages Remaining",
+        "Blog pages",
+        "Extra Pages",
+      ],
+      required: true,
+    },
+    dateComplete: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true, // adds createdAt and updatedAt fields automatically
+  }
+);
+
+// Create the model
+const CopywriterTracker = model("CopywriterTracker", copywriterTrackerSchema);
+
+export default CopywriterTracker;
